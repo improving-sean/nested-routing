5871b2b543f95f22f2d29b0a165340b817dd4782
Sean Norton <sean.norton@improving.com>
2022-03-28 10:20:18 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nested-routing',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
